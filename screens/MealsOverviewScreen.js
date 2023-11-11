d173f34ecd0feffd7dc6ff063dc13aa1650de55a
Ikhbar Nurinsani <ikhbarnuri@gmail.com>
2023-11-11 17:24:17 +0700

    feat: add categories screen and meals overview screen

---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,45 @@
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
+
+const MealsOverviewScreen = ({ route }) => {
+  const categoryId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((meatItem) => {
+    return meatItem.categoryIds.indexOf(categoryId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    const { title, imageUrl, duration, complexity, affordability } = itemData.item;
+
+    return (
+      <MealItem
+        title={title}
+        imageUrl={imageUrl}
+        duration={duration}
+        complexity={complexity}
+        affordability={affordability}
+      />
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={displayedMeals}
+        renderItem={renderMealItem}
+        keyExtractor={(item) => item.id}
+        style={{ paddingHorizontal: 16 }}
+      />
+    </View>
+  );
+};
+
+export default MealsOverviewScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingBottom: 16,
+  },
+});
